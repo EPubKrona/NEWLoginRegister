0da930fb0af709d41a936dd6e58b0cd8d3d88805
EPubKrona <stephan_kronawitter@web.de>
2017-05-10 10:56:28 +0200

    Update Register.php

--- Register.php
+++ Register.php
@@ -1,13 +1,13 @@
 <?php
-    $con = mysqli_connect("mysql10.000webhost.com", "id1589479_krona", "shortyMP3", "id1589479_epub");
+    $con = mysqli_connect("files.000webhost.com", "id1612358_krona", "shortyMP3", "id1612358_epub");
     
     $name = $_POST["name"];
-    $age = $_POST["age"];
+    $age = $_POST["mail"];
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    $statement = mysqli_prepare($con, "INSERT INTO user (name, username, age, password) VALUES (?, ?, ?, ?)");
-    mysqli_stmt_bind_param($statement, "siss", $name, $username, $age, $password);
+    $statement = mysqli_prepare($con, "INSERT INTO user (name, username, mail, password) VALUES (?, ?, ?, ?)");
+    mysqli_stmt_bind_param($statement, "siss", $name, $username, $mail, $password);
     mysqli_stmt_execute($statement);
     
     $response = array();
