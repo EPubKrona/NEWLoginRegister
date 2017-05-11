5b8778f976b10f22f94bf9ffac7d690df1530f20
EPubKrona <stephan_kronawitter@web.de>
2017-05-11 14:28:39 +0200

    Update Register.php

--- Register.php
+++ Register.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("85.13.130.218", "d026557a", "shortyMP3", "d026557a");
+    $con = mysqli_connect("d026557a.all-inkl.com/", "d026557a", "shortyMP3", "d026557a");
     
     $name = $_POST["name"];
     $mail = $_POST["mail"];
