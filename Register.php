cb2ce6f02f2f1d83369c8e649cb84f546f397458
EPubKrona <stephan_kronawitter@web.de>
2017-05-11 10:21:32 +0200

    Update Register.php

--- Register.php
+++ Register.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("test94963.test-account.com", "w016e7fd", "UQFnpodK43Ev", "d026557a");
+    $con = mysqli_connect("localhost", "w016e7fd", "UQFnpodK43Ev", "d026557a");
     
     $name = $_POST["name"];
     $age = $_POST["mail"];
